56fe8e3c5f75dfa80104262a915f42caf196a412
matomaalejandro441-web <matomaalejandro441@gmail.com>
2026-06-26 11:16:47 -0500

    career create

--- routers/career.py
+++ routers/career.py
@@ -10,4 +10,24 @@ def module_career_config():
         return render_template("Career_config.html", career=career, user=users[id_actual])
     return redirect(url_for("home"))
 
-   
+@career_bp.route("/Create_token", methods=["GET", "POST"])
+def Create_token():
+
+    career_avaliable = career.get("career")
+
+    id_actual = session.get('user_id')
+
+    if request.method == 'POST' and id_actual in users:
+
+        new_career = request.form.get('career')
+
+        if new_career in career:
+
+
+            return redirect(url_for("coordinador.coordinador"))
+        
+    return render_template(
+        "C_Create_Career.html",
+        career_avaliable=career_avaliable,
+        user = users[id_actual]
+    )
